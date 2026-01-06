f80c4e72a000637be47add5e35c88fddcb201150
gyu <sdsdcs2013@gmail.com>
2026-01-07 08:46:06 +0900

    도메인 설정

--- qplan-docs/astro.config.mjs
+++ qplan-docs/astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://qplan-lang.github.io",
-  base: "/qplan/",
+  site: "https://www.qplan.org",
+  base: "/",
   integrations: [react()]
 });
