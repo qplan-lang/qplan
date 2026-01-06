629ea937fdf560afa5176c37af52cfabca1184ff
gyu <sdsdcs2013@gmail.com>
2026-01-07 08:15:34 +0900

    site base 경로 변경

--- qplan-docs/astro.config.mjs
+++ qplan-docs/astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://qplan-lang.github.io",
+  base: "/qplan/",
   integrations: [react()]
 });
